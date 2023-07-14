d98ce5460efc95eccfd73405e9ae6257f75d3e06
timer <330070781@qq.com>
2023-07-14 11:45:31 +0800

    7.14:va_list 在riscv32架构下测试

--- test/va_list/build.sh
+++ test/va_list/build.sh
@@ -1,10 +1,13 @@
 
 
-gcc -O0 -m32 -fno-builtin -fno-pic -fno-pie  -fno-stack-protector  va_list.c -o i386.out
-riscv64-unknown-elf-gcc -O0 -fno-builtin -fno-pic -fno-pie -fno-stack-protector va_list.c -o riscv64.out
+gcc -O0 -m32   va_list.c -o i386.out
+riscv64-unknown-elf-gcc -O0 va_list.c -o riscv64.out
+riscv32-unknown-elf-gcc -O0   va_list.c -o riscv32.out
 
 objdump -D i386.out > i386_debug.txt
 riscv64-unknown-elf-objdump -D riscv64.out > riscv64_debug.txt
+riscv32-unknown-elf-objdump -D riscv32.out > riscv32_debug.txt
 
 #   qemu-i386 i386.out
 #   qemu-riscv64 riscv64.out
+#   qemu-riscv32 riscv32.out
